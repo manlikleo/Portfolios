8dfe7e0a86832baad1369761ff298169fa1cf290
olaotan lawal <olaotan104@gmail.com>
2020-06-21 13:39:45 +0100

    added image

--- scripts/contact.php
+++ scripts/contact.php
@@ -1,7 +1,7 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	if(!empty($_POST['contact_name']) && !empty($_POST['contact_email']) && !empty($_POST['contact_message'])) {
-		$to = 'email@address.com'; // Replace with your email.
+		$to = 'olaotan@address.com'; // Replace with your email.
 		$body = "Name: {$_POST['contact_name']}\n\nEmail: {$_POST['contact_email']}\n\nMessage: {$_POST['contact_message']}";
 		mail($to, "Contact Form Submission", $body, "From: {$_POST['contact_email']}");
 	}
